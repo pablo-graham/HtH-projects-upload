08f354531a5c19d2d684d2b8570cb614c7763586
Pablo Ruiz <pabloruiz@Pablos-MacBook-Pro.local>
2021-11-09 10:25:45 -0800

    list and dictionaries work

--- Python-List-and-Dictionaries/dictionary_practice.py
+++ Python-List-and-Dictionaries/dictionary_practice.py
@@ -14,19 +14,39 @@ foodPrices["Apples"] = .20
 foodPrices["Soda"] = 2.00
 foodPrices["Cereal"] = 4.00
 
-print(chickenPrices)
-print(beefPrices)
-print(cheesePrices)
-print(milkPrices)
-print(foodPrices["Apples"])
-print(foodPrices["Soda"])
-print(foodPrices["Cereal"])
+# print(chickenPrices)
+# print(beefPrices)
+# print(cheesePrices)
+# print(milkPrices)
+# print(foodPrices["Apples"])
+# print(foodPrices["Soda"])
+# print(foodPrices["Cereal"])
 
 foodPrices.pop("Chicken")
-foodPrices.pop("Beef")
+foodPrices.pop("Milk")
 
 # print(foodPrices)
 
+# ------------------------- still need to do the last stretch -------------------------
+def total_price(item1, item2) :
+    if item1 in foodPrices and item2 in foodPrices:
+        total = foodPrices[item1] + foodPrices[item2]
+        print("The total price of beef and cheese is {}".format(total))
+    else:
+        print("that food is not available")
+
+# total_price("Beef", "Cheese")
+
+def price_difference(item1, item2):
+    if item1 in foodPrices and item2 in foodPrices:
+        dif = foodPrices[item1] - foodPrices[item2]
+        if dif < 0:
+            dif *= -1
+        print("The difference in price of beef and cheese is {}".format(dif))
+    else:
+        print("that food is not available")
+
+# price_difference("Cheese", "Beef")
 
 
 favorites = {
@@ -66,30 +86,79 @@ shoeInventory = {
     "Airmax" : 5,
     "SB Dunk" : 20
 }
-
+# print(shoeInventory)
 shoeInventory["SB Dunk"] -= 2
 shoeInventory["Yeezy"] += 1
-
-
+# print(shoeInventory)
 shoeInventory["Jordan 13"] += 7
-shoeInventory["Yeezy"] =+ 7
+shoeInventory["Yeezy"] = shoeInventory["Yeezy"] + 7
 shoeInventory["Foamposite"] += 7
 shoeInventory["Airmax"] += 7
 shoeInventory["SB Dunk"] += 7
+# print(shoeInventory)
 
 shoeInventory["Jordan 13"] -= 3
 shoeInventory["Yeezy"] -= 3
 shoeInventory["Foamposite"] -= 3
 shoeInventory["Airmax"] -= 3
 shoeInventory["SB Dunk"] -= 3
+# print(shoeInventory)
 
 shoeInventory["Travis Scott AF1"] = 1
 shoeInventory["Vans"] = 3
 shoeInventory["Cleats"] = 2
 
-# print(shoeInventory)
-
-shoeInventory.pop("Yeezy")
+shoeInventory.pop("Foamposite")
 shoeInventory.pop("Airmax")
 
-# print(shoeInventory)
+def restock(item1, multi) :
+    if item1 == "All":
+            shoeInventory["Vans"] *= multi
+            shoeInventory["Jordan 13"] *= multi
+            shoeInventory["Travis Scott AF1"] *= multi
+            shoeInventory["Cleats"] *= multi
+            shoeInventory["SB Dunk"] *= multi
+            shoeInventory["Yeezy"] *= multi
+            print("The new inventory is {}".format(shoeInventory))
+    elif item1 in shoeInventory:
+        shoeInventory[item1] *= multi
+        print("The new inventory is {}".format(shoeInventory))
+    else:
+        print("that shoe isn't available")
+
+# restock("All", 3)
+
+
+def clearance_sale(item1, sale):
+    if item1 == "All":
+            shoeInventory["Vans"] /= sale
+            shoeInventory["Jordan 13"] /= sale
+            shoeInventory["Travis Scott AF1"] /= sale
+            shoeInventory["Cleats"] /= sale
+            shoeInventory["SB Dunk"] /= sale
+            shoeInventory["Yeezy"] /= sale
+            print("The new inventory is {}".format(shoeInventory))
+    elif item1 in shoeInventory:
+        shoeInventory[item1] = shoeInventory[item1] / sale
+        print("The new inventory {}".format(shoeInventory))
+    else:
+        print("that shoe is not available")
+
+# clearance_sale("a", 2)
+
+
+playersInSports = {
+    "Soccer" : 11,
+    "Basketball" : 5,
+    "Baseball" : 9,
+    "Football" : 11
+}
+
+def injuredPlayers(sport, numOfInjPlayers):
+    if numOfInjPlayers == playersInSports[sport]:
+         print("All your players are injured")
+    elif numOfInjPlayers > playersInSports[sport]:
+        print("how did you injured more players than you have?")
+    else:
+        print("{} number of not injured players left in your sport").format(playersInSports[sport] - numOfInjPlayers)
+injuredPlayers("Soccer", 6)
--- Python-List-and-Dictionaries/list_practice.py
+++ Python-List-and-Dictionaries/list_practice.py
@@ -23,6 +23,38 @@ cities.pop(5)
 cities.remove("Denver")
 # print(cities)
 
+def printCities():
+    for i in range(len(cities)):
+        if(i == 0):
+            print(cities[i])
+        elif len(cities[i]) > len(cities[i-1]):
+            print(cities[i])
+
+    while i <= (len(cities)):
+        if(i == 0):
+            print(cities[i])
+        elif len(cities[i]) > len(cities[i-1]):
+            print(cities[i])
+        i +=1
+
+# printCities()
+
+def sortCities():
+    for i in range(len(cities)):
+        if i == len(cities) - 1:
+            return cities
+        elif len(cities[i]) <= len(cities[i+1]):
+            continue
+        else:
+            short = cities[i + 1]
+            cities.pop(i + 1)
+            cities.append(short)
+
+    return cities
+
+print(sortCities())
+# need help in the last STRETCH of step x
+# need help commbine functions
 
 letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
 # print(letters) 
